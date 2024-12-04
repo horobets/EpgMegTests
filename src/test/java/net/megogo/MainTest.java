e2a7823ec965ecfe51e757e721b40d1bbb83c7e6
Serhii <40602367+horobets@users.noreply.github.com>
2024-12-05 00:04:48 +0200

    Update MainTest.java

--- src/test/java/net/megogo/MainTest.java
+++ src/test/java/net/megogo/MainTest.java
@@ -1,7 +1,30 @@
 package net.megogo;
 
+import io.qameta.allure.Step;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class MainTest {
+
+    private static final Logger log = Logger.getLogger(MainTest.class);
+
+    @Test
+    public void sampleTet(){
+
+        doM();
+        Assert.assertTrue(true);
+
+    }
+
+    @Step("Do important action")
+    private void doM(){
+
+        System.gc();
+
+    }
+
 
 }
