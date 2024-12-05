c5e5ed306a5e5e9f0821a8735597213c291b8775
Serhii Horobets <40602367+horobets@users.noreply.github.com>
2024-12-05 16:10:47 +0200

    assert request added

--- src/test/java/net/megogo/api/MegApiTestsFixture.java
+++ src/test/java/net/megogo/api/MegApiTestsFixture.java
@@ -1,6 +1,9 @@
 package net.megogo.api;
 
+import io.qameta.allure.Step;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import retrofit2.Response;
 
 public class MegApiTestsFixture {
 
@@ -9,5 +12,12 @@ public class MegApiTestsFixture {
     @BeforeClass
     public void beforeClass() {
         megogoRestClient = new MegogoRestClient();
+    }
+
+    @Step("Assert API Request")
+    protected void assertApiRequest(Response<?> response){
+        Assert.assertTrue(response.isSuccessful(), "API request failed");
+        Assert.assertEquals(response.code(), 200, "Received an unexpected HTTP status code");
+        Assert.assertNotNull(response.body(), "Request body is null, but it was expected to contain specific information");
     }
 }
