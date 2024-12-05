ee88522a472123f2dee9c56263e8cab38835b98a
Serhii <40602367+horobets@users.noreply.github.com>
2024-12-05 12:50:26 +0200

    Sorting check added

--- src/test/java/net/megogo/api/QueryingChannelDataTest.java
+++ src/test/java/net/megogo/api/QueryingChannelDataTest.java
@@ -4,44 +4,70 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
 import io.qameta.allure.Story;
+import net.megogo.api.models.Program;
+import net.megogo.api.utils.RequestUtilInterceptor;
 import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.temporal.ChronoField;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @Epic("Channel Data Querying")
-public class QueryingChannelDataTest {
+public class QueryingChannelDataTest extends MegApiTestsFixture {
 
-    private static final Logger log = Logger.getLogger(QueryingChannelDataTest.class);
-    private MegogoRestClient megogoRestClient;
-
-    @BeforeClass
-    public void beforeClass() {
-        megogoRestClient = new MegogoRestClient();
-    }
+    private static final Logger logger = Logger.getLogger(QueryingChannelDataTest.class);
 
     @Test
-    @Feature("User critical features")
-    @Story("Processing Time")
-    public void sampleTet() throws IOException {
+    @Feature("Current Time Provider")
+    @Story("Return the Accurate Current Time")
+    public void sampleTest() throws IOException {
+
+        var startTime = Instant.now();
         var timeResult = megogoRestClient.megogoScheduleService.getTime().execute().body();
-        System.out.println(timeResult);
+        var endTime = Instant.now();
+
+        logger.info("%nStart time: %s  End time: %s%nReturned time: %s%n".formatted( startTime.getEpochSecond(), endTime.getEpochSecond(),timeResult));
+
         doM();
 
         Assert.assertTrue(true);
     }
 
-    @Test
-    @Feature("Channel Data Sorting")
+    @Test(dataProvider = "test-channels-list")
+    @Parameters({"channelId"})
+    @Feature("Channel Data Provider")
     @Story("Check Channel Data Sorting")
-    public void channelDataSortingTest() throws IOException {
-        var channelResults = megogoRestClient.megogoScheduleService.getChannel("1639111,1585681").execute().body();
-        System.out.println(channelResults);
-        doM();
+    public void channelDataSortingTest(String channelId) throws IOException {
+        var channelDataRequestResult = megogoRestClient.megogoScheduleService.getChannel(channelId).execute();
+        Assert.assertTrue(channelDataRequestResult.isSuccessful(), "Get channel data request failed");
+        Assert.assertEquals(channelDataRequestResult.code(), 200, "Received an unexpected HTTP status code");
+        Assert.assertNotNull(channelDataRequestResult.body(), "Request body is null, but it was expected to contain specific information");
+        channelDataRequestResult.body().data().forEach(channelData -> assertProgramsSorted(channelData.programs()));
+    }
 
-        Assert.assertTrue(true);
+    @Step("Assert programs sorted")
+    private void assertProgramsSorted(List<Program> programs){
+        var sortedPrograms = new ArrayList<Program>(programs);
+        sortedPrograms.sort(Comparator.comparingInt(Program::startTimestamp));
+        Assert.assertEquals(programs, sortedPrograms, "The programs are not sorted as expected");
+    }
+
+    @DataProvider(name = "test-channels-list",parallel=true)
+    public Object[][] channelsTestData() {
+        return new Object[][] {
+                new Object[] { "1639111" },
+                new Object[] { "1585681" },
+                new Object[] { "1639231" }
+        };
     }
 
     @Test
