ce9bffa2b5337a7c0adba994a117c43d21477540
Serhii <40602367+horobets@users.noreply.github.com>
2024-12-05 20:53:30 +0200

    Update MegogoRestClient.java

--- src/main/java/net/megogo/api/MegogoRestClient.java
+++ src/main/java/net/megogo/api/MegogoRestClient.java
@@ -5,21 +5,19 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
-import java.util.concurrent.TimeUnit;
-
 public class MegogoRestClient {
 
-    public static final String BASE_URL = "https://epg.megogo.net/";
+    private static final String BASE_URL = "https://epg.megogo.net/";
 
-    OkHttpClient client = new OkHttpClient.Builder()
+    private OkHttpClient client = new OkHttpClient.Builder()
             .addInterceptor(new RequestUtilInterceptor())
             .build();
 
-    Retrofit retrofit = new Retrofit.Builder()
+    private Retrofit retrofit = new Retrofit.Builder()
             .client(client)
             .baseUrl(BASE_URL)
             .addConverterFactory(JacksonConverterFactory.create())
             .build();
 
-    public MegogoScheduleService megogoScheduleService = retrofit.create(MegogoScheduleService.class);
+    public final MegogoScheduleService megogoScheduleService = retrofit.create(MegogoScheduleService.class);
 }
